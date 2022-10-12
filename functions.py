d67a9fea3863f295fa3024dff08388fd3681ea07
1822913 <peterkoegler@gmx.de>
2022-10-12 18:07:03 +0200

    Exercise 1

--- functions.py
+++ functions.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
-print("Test")
+
+def create_corners(width):
+    return 2*("#"+width*"-")+"#"+"\n"
+
+def create_central(width):
+    return 2*("|"+width*" ")+"|"+"\n"
+
+def create_grid(squares, width, height):
+    return squares*(create_corners(width)+height*create_central(width))+create_corners(width)
+
+print(create_grid(5, 8, 1))
